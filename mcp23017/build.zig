8ca05438ddf9e743fc834919d24155c15893a6f9
Mohan Gupta <mohangupta@live.com>
2026-05-12 19:38:46 +1000

    mcp23017 zig 0.16

--- mcp23017/build.zig
+++ mcp23017/build.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // This is the equivalent of clang's `--target=wasm32-unknown-wasi`
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .wasm32,
         .os_tag = .wasi,
@@ -9,19 +8,33 @@ pub fn build(b: *std.Build) void {
     });
 
     const optimize = b.standardOptimizeOption(.{
-        .preferred_mode = .ReleaseSmall,
+        .preferred_optimize_mode = .ReleaseSmall,
     });
 
-    const mod = b.addModule("chip_zig", .{
-        .root_source_file = b.path("chip.zig"),
-        .target = target,
-        .optimize = optimize,
+    const exe = b.addExecutable(.{
+        .name = "chip",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("chip.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
-    mod.install();
-    //lib.setTarget(target);
-    //lib.setBuildMode(mode);
-    //lib.addPackagePath("wokwi", "wokwi/wokwi_chip_ll.zig");
-    //lib.export_table = true;
-    //lib.install();
+    exe.export_table = true;
+    exe.rdynamic = true;
+    exe.entry = .disabled;
+
+    const install_step = b.addUpdateSourceFiles();
+    install_step.addCopyFileToSource(exe.getEmittedBin(), "dist/chip.wasm");
+    b.getInstallStep().dependOn(&install_step.step);
+
+    // const mode = b.standardReleaseOptions();
+    // const target: std.zig.CrossTarget = .{ .cpu_arch = .wasm32, .os_tag = .freestanding };
+
+    // const lib = b.addSharedLibrary("chip_zig", "src/lib.zig", .unversioned);
+    // lib.setTarget(target);
+    // lib.setBuildMode(mode);
+    // lib.addPackagePath("wokwi", "wokwi/wokwi_chip_ll.zig");
+    // lib.export_table = true;
+    // lib.install();
 }
